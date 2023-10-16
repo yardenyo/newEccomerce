3bda672ac57a47fc3ce861ce5bf13d0461bb95a0
yardenyo <gmloler@gmail.com>
2023-10-16 18:42:58 +0300

    remove unused code

--- server/src/utils/refreshToken.ts
+++ server/src/utils/refreshToken.ts
@@ -1,6 +1,5 @@
 import jwt from 'jsonwebtoken';
 
-// Generate refresh token
 export const generateRefreshToken = (id: string) => {
     return jwt.sign({ id }, process.env.JWT_SECRET!, {
         expiresIn: '3d',
