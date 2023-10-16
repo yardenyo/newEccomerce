80bbad161a9291fd74ca80b2ec6e47d544403016
yardenyo <gmloler@gmail.com>
2023-10-16 23:36:05 +0300

    regex for password

--- server/src/resources/auth/auth.validation.ts
+++ server/src/resources/auth/auth.validation.ts
@@ -5,7 +5,15 @@ const signup = Joi.object({
     lastName: Joi.string().required(),
     email: Joi.string().email().required(),
     mobile: Joi.string().required(),
-    password: Joi.string().required(),
+    password: Joi.string()
+        .required()
+        .min(8)
+        .max(32)
+        .pattern(
+            new RegExp(
+                '^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,32}$',
+            ),
+        ),
 });
 
 const signin = Joi.object({
