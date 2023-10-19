4fa5439452de54ecf3c0d43e93633a7cc00eb79c
yardenyo <gmloler@gmail.com>
2023-10-19 20:46:46 +0300

    category controller

---
+++ server/src/resources/category/category.controller.ts
@@ -0,0 +1,153 @@
+import { Router, Request, Response, NextFunction } from 'express';
+import Controller from '@/utils/interfaces/controller.interface';
+import HttpException from '@/utils/exceptions/http.exception';
+import SuccessResponse from '@/middleware/success.middleware';
+import validationMiddleware from '@/middleware/validation.middleware';
+import validate from '@/resources/category/category.validation';
+import CategoryService from '@/resources/category/category.service';
+import {
+    authMiddleware,
+    creatorMiddleware,
+} from '@/middleware/auth.middleware';
+import validateDBId from '@/utils/validateDBId';
+
+class CategoryController implements Controller {
+    public path = '/categories';
+    public router = Router();
+    private categoryService = new CategoryService();
+
+    constructor() {
+        this.initializeRoutes();
+    }
+
+    private initializeRoutes(): void {
+        this.router.post(
+            `${this.path}/create`,
+            validationMiddleware(validate.createCategory),
+            authMiddleware,
+            creatorMiddleware,
+            this.createCategory,
+        );
+        this.router.post(`${this.path}`, authMiddleware, this.getAllCategories);
+        this.router.get(
+            `${this.path}/:id`,
+            authMiddleware,
+            this.getCategoryById,
+        );
+        this.router.put(
+            `${this.path}/:id`,
+            validationMiddleware(validate.updateCategory),
+            authMiddleware,
+            creatorMiddleware,
+            this.updateCategory,
+        );
+        this.router.delete(
+            `${this.path}/:id`,
+            authMiddleware,
+            creatorMiddleware,
+            this.deleteCategory,
+        );
+        this.router.delete(
+            `${this.path}`,
+            authMiddleware,
+            creatorMiddleware,
+            this.deleteAllCategories,
+        );
+    }
+
+    private createCategory = async (
+        req: Request,
+        res: Response,
+        next: NextFunction,
+    ): Promise<void> => {
+        try {
+            const categoryData = req.body;
+            const newCategory =
+                await this.categoryService.createCategory(categoryData);
+            res.json(new SuccessResponse('Category created', newCategory));
+        } catch (error: any) {
+            next(new HttpException(400, error.message));
+        }
+    };
+
+    private getAllCategories = async (
+        req: Request,
+        res: Response,
+        next: NextFunction,
+    ): Promise<void> => {
+        try {
+            const categories = await this.categoryService.getAllCategories(
+                req.body,
+            );
+            res.json(new SuccessResponse('Categories found', categories));
+        } catch (error: any) {
+            next(new HttpException(400, error.message));
+        }
+    };
+
+    private getCategoryById = async (
+        req: Request,
+        res: Response,
+        next: NextFunction,
+    ): Promise<void> => {
+        try {
+            const categoryId = req.params.id;
+            await validateDBId(categoryId);
+            const category =
+                await this.categoryService.getCategoryById(categoryId);
+            res.json(new SuccessResponse('Category found', category));
+        } catch (error: any) {
+            next(new HttpException(400, error.message));
+        }
+    };
+
+    private updateCategory = async (
+        req: Request,
+        res: Response,
+        next: NextFunction,
+    ): Promise<void> => {
+        try {
+            const categoryId = req.params.id;
+            const categoryData = req.body;
+            await validateDBId(categoryId);
+            const updatedCategory = await this.categoryService.updateCategory(
+                categoryId,
+                categoryData,
+            );
+            res.json(new SuccessResponse('Category updated', updatedCategory));
+        } catch (error: any) {
+            next(new HttpException(400, error.message));
+        }
+    };
+
+    private deleteCategory = async (
+        req: Request,
+        res: Response,
+        next: NextFunction,
+    ): Promise<void> => {
+        try {
+            const categoryId = req.params.id;
+            await validateDBId(categoryId);
+            const deletedCategory =
+                await this.categoryService.deleteCategory(categoryId);
+            res.json(new SuccessResponse('Category deleted', deletedCategory));
+        } catch (error: any) {
+            next(new HttpException(400, error.message));
+        }
+    };
+
+    private deleteAllCategories = async (
+        req: Request,
+        res: Response,
+        next: NextFunction,
+    ): Promise<void> => {
+        try {
+            await this.categoryService.deleteAllCategories();
+            res.json(new SuccessResponse('All categories deleted'));
+        } catch (error: any) {
+            next(new HttpException(400, error.message));
+        }
+    };
+}
+
+export default CategoryController;
---
+++ server/src/resources/category/category.service.ts
@@ -0,0 +1,84 @@
+import Category from '@/resources/category/category.interface';
+import CategoryModel from '@/resources/category/category.model';
+import PostBody from '@/utils/interfaces/postbody.interface';
+import ConvertResponse from '@/utils/helpers/convertresponse.helper';
+
+class CategoryService {
+    private category = CategoryModel;
+
+    public async createCategory(categoryData: Category): Promise<Category> {
+        try {
+            const createdCategory = this.category.create(categoryData);
+            return createdCategory;
+        } catch (error) {
+            throw new Error('Error creating category');
+        }
+    }
+
+    public async getAllCategories(body: PostBody): Promise<Category[]> {
+        try {
+            const { sort, skip, limit, searchFilter } =
+                await ConvertResponse(body);
+
+            const categories = await this.category
+                .find(searchFilter)
+                .sort(sort)
+                .skip(skip)
+                .limit(limit);
+
+            return categories;
+        } catch (error) {
+            throw new Error('Error retrieving categories');
+        }
+    }
+
+    public async getCategoryById(id: string): Promise<Category> {
+        try {
+            const category = await this.category.findById(id);
+            if (!category) throw new Error();
+
+            return category;
+        } catch (error) {
+            throw new Error('Error retrieving category');
+        }
+    }
+
+    public async updateCategory(
+        id: string,
+        categoryData: Category,
+    ): Promise<Category> {
+        try {
+            const category = await this.category.findByIdAndUpdate(
+                id,
+                categoryData,
+                { new: true },
+            );
+            if (!category) throw new Error();
+
+            return category;
+        } catch (error) {
+            throw new Error('Error updating category');
+        }
+    }
+
+    public async deleteCategory(id: string): Promise<Category> {
+        try {
+            const category = await this.category.findByIdAndDelete(id);
+            if (!category) throw new Error();
+
+            return category;
+        } catch (error) {
+            throw new Error('Error deleting category');
+        }
+    }
+
+    public async deleteAllCategories(): Promise<void> {
+        try {
+            await this.category.deleteMany({});
+        } catch (error) {
+            throw new Error('Error deleting categories');
+        }
+    }
+}
+
+export default CategoryService;
