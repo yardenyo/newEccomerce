32438c2b90f60fe9a604a706ba9a18b9049df85b
yardenyo <gmloler@gmail.com>
2023-11-01 22:20:23 +0200

    removed dark mode for now

--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -1,23 +1,15 @@
 import { Outlet } from "react-router-dom";
-import { selectCurrentUser } from "@/features/auth/authSlice";
-import { useSelector } from "react-redux";
 import Navbar from "@/components/Navbar";
 import Notice from "@/components/Notice";
 import { useState } from "react";
 
 const Layout = () => {
-  const user = useSelector(selectCurrentUser);
-  const isDarkMode = user?.userSettings?.settings?.darkMode;
   const [showNotice, setShowNotice] = useState(true);
 
   return (
     <main className="font-body">
       <Notice showNotice={showNotice} setShowNotice={setShowNotice} />
-      <div
-        className={`App${isDarkMode ? " dark" : ""} container mx-auto ${
-          showNotice && "pt-10"
-        } px-4`}
-      >
+      <div className={`App container mx-auto ${showNotice && "pt-10"} px-4`}>
         <Navbar />
         <Outlet />
       </div>
